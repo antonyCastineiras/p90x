cb2c5614b0583d5546c7a9601bf9173adf1c5265
antonyCastineiras <antonycastineiras80@gmail.com>
2017-06-06 22:29:05 +0100

    exercises are created when a workout is created

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,83 @@
 class Exercise < ApplicationRecord
 	belongs_to :workout
+
+	def self.balance_lunges
+		{name: 'balance_lunges'}
+	end
+
+	def self.calf_raise_squats
+		{name: 'calf_raise_squats'}
+	end
+
+	def self.reverse_grip_pull_ups
+		{name: 'reverse_grip_pull_ups'}
+	end
+
+	def self.super_skaters
+		{name: 'super_skaters'}
+	end
+
+	def self.wall_squats
+		{name: 'wall_squats'}
+	end
+
+	def self.wide_grip_pull_ups
+		{name: 'wide_grip_pull_ups'}
+	end
+
+	def self.step_back_lunges
+		{name: 'step_back_lunges'}
+	end
+
+	def self.alternating_side_lunges
+		{name: 'alternating_side_lunges'}
+	end
+
+	def self.close_grip_pull_ups
+		{name: 'close_grip_pull_ups'}
+	end
+
+	def self.single_leg_wall_squat
+		{name: 'single_leg_wall_squat'}
+	end
+
+	def self.dead_lift_squats
+		{name: 'dead_lift_squats'}
+	end
+
+	def self.switch_grip_pull_ups
+		{name: 'switch_grip_pull_ups'}
+	end
+
+	def self.three_way_lunges
+		{name: 'three_way_lunges'}
+	end
+
+	def self.sneaky_lunges
+		{name: 'sneaky_lunges'}
+	end
+
+	def self.chair_salutations
+		{name: 'chair_salutations'}
+	end
+
+	def self.toe_roll_iso_lunges
+		{name: 'toe_roll_iso_lunges'}
+	end
+
+	def self.wide_front_pull_ups
+		{name: 'wide_front_pull_ups'}
+	end
+
+	def self.groucho_walk
+		{name: 'groucho_walk'}
+	end
+
+	def self.calf_raises
+		{name: 'calf_raises'}
+	end
+
+	def self.eighty_twenty_speed_squats
+		{name: 'eighty_twenty_speed_squats'}
+	end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,10 +1,22 @@
 class Workout < ApplicationRecord
 	belongs_to :user
-	has_many :exercises
+	has_many :exercises, dependent: :destroy
 
 	validates :name, presence: true
-	after_validation :create_exercises
+	after_save :create_exercises
 
 	def create_exercises
+		exercise_names = get_exercise_names
+		exercise_names.each  {|exercise_name|
+			Exercise.create({name: exercise_name, workout: self})
+		} 
+	end
+
+	def get_exercise_names
+		self.send(self.name + '_exercises')
+	end
+
+	def legs_and_back_exercises
+		[:balance_lunges, :calf_raise_squats, :reverse_grip_pull_ups, :super_skaters, :wall_squats, :wide_grip_pull_ups, :step_back_lunges, :alternating_side_lunges, :close_grip_pull_ups, :single_leg_wall_squat, :dead_lift_squats, :switch_grip_pull_ups, :three_way_lunges, :sneaky_lunges, :reverse_grip_pull_ups, :chair_salutations, :toe_roll_iso_lunges, :wide_front_pull_ups, :groucho_walk, :calf_raises, :close_grip_pull_ups, :eighty_twenty_speed_squats, :switch_grip_pull_ups]
 	end
 end
