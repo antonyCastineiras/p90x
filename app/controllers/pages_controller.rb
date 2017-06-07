2c2404dbff324d8ca23d2d757870b7ea86c945b8
antonyCastineiras <antonycastineiras80@gmail.com>
2017-06-07 18:23:51 +0100

    redirects to create workout page after sign up/in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-	include UserHelper
-
   def home
+  	redirect_to new_workout_path if current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  devise_for :users
+  resources :workouts
+
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
